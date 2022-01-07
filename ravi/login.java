9e7acc71fb85ff0de2df1e2edb0233b584e1df15
sisindri <sisindribabu98@gmail.com>
2022-01-07 11:58:49 +0530

    firs

--- ravi/login.java
+++ ravi/login.java
@@ -1,14 +1,11 @@
-public class ReverseNumberExample1   
-{  
-public static void main(String[] args)   
-{  
-int number = 987654, reverse = 0;  
-while(number != 0)   
-{  
-int remainder = number % 10;  
-reverse = reverse * 10 + remainder;  
-number = number/10;  
-}  
-System.out.println("The reverse of the given number is: " + reverse);  
-}  
-}  
+public class Login
+{
+	public static void main(String args[])
+	{
+		int i;
+		for(i=0;i<=10;i++)
+		{
+			system.out.println("number:"+i);
+		}
+	}
+}
